c22f8f4427e59b2af1cbd276c157c40deda64465
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2018-01-24 20:40:02 +0100

    Added addProduct method and test for it

---
+++ src/main/java/com/kchmielewski/sda/java6/shophop/model/Order.java
@@ -0,0 +1,46 @@
+package com.kchmielewski.sda.java6.shophop.model;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class Order {
+    private BigDecimal totalPrice = BigDecimal.ZERO;
+    private Map<Product, Integer> products = new LinkedHashMap<>();
+
+
+    public Map<Product, Integer> getProducts() {
+        return Collections.unmodifiableMap(products);
+//        return new LinkedHashMap<>(products);
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void addProduct(Product product) {
+        addProduct(product, 1);
+    }
+
+    public void addProduct(Product product, int quantity) {
+        checkNotNull(product, "Product cannot be null");
+        checkArgument(quantity > 0, "Quantity must be positive: %s", quantity);
+
+        if (products.containsKey(product)) {
+            products.put(product, products.get(product) + quantity);
+        } else {
+            products.put(product, quantity);
+        }
+
+        calculateTotalPrice(product, quantity);
+    }
+
+    private void calculateTotalPrice(Product product, int quantity) {
+        totalPrice = totalPrice.add(product.getPrice().multiply(BigDecimal.valueOf(quantity)));
+    }
+
+}
---
+++ src/test/java/com/kchmielewski/sda/java6/shophop/model/OrderTest.java
@@ -0,0 +1,76 @@
+package com.kchmielewski.sda.java6.shophop.model;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+public class OrderTest {
+    private final Order order = new Order();
+    private final Product firstProduct = mock(Product.class);
+    private final Product secondProduct = mock(Product.class);
+
+    @Test
+    public void addProductThrowsExceptionForNullProduct() {
+        assertThatThrownBy(() -> order.addProduct(null)).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void addProductThrowsExceptionForZeroQuantity() {
+        assertThatThrownBy(() -> order.addProduct(firstProduct, 0)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void checkIfNonExistentProductIsAdded() {
+        given(firstProduct.getPrice()).willReturn(BigDecimal.ONE);
+
+        order.addProduct(firstProduct, 5);
+
+        assertThat(order.getProducts()).containsEntry(firstProduct, 5);
+    }
+
+    @Test
+    public void checkIfExistentProductQuantityIsIncreased() {
+        given(firstProduct.getPrice()).willReturn(BigDecimal.ONE);
+
+        order.addProduct(firstProduct, 5);
+        order.addProduct(firstProduct);
+
+        assertThat(order.getProducts()).containsEntry(firstProduct, 6);
+    }
+
+    @Test
+    public void forNewProductTotalPriceEqualsProductPriceTimesQuantity() {
+        given(firstProduct.getPrice()).willReturn(BigDecimal.valueOf(3.20));
+
+        order.addProduct(firstProduct, 3);
+
+        assertThat(order.getTotalPrice()).isEqualByComparingTo(BigDecimal.valueOf(9.60));
+    }
+
+    @Test
+    public void forSameProductAddedTwiceTotalPriceIsIncreased() {
+        given(firstProduct.getPrice()).willReturn(BigDecimal.valueOf(3.20));
+
+        order.addProduct(firstProduct, 3);
+        order.addProduct(firstProduct, 3);
+
+        assertThat(order.getTotalPrice()).isEqualByComparingTo(BigDecimal.valueOf(19.20));
+    }
+
+    @Test
+    public void forTwoDifferentProductsTotalPriceEqualsQuantityTimesTheirPrice() {
+        given(firstProduct.getPrice()).willReturn(BigDecimal.valueOf(3.20));
+        given(secondProduct.getPrice()).willReturn(BigDecimal.valueOf(4.20));
+
+        order.addProduct(firstProduct, 2);
+        order.addProduct(secondProduct, 2);
+
+        assertThat(order.getTotalPrice()).isEqualByComparingTo(BigDecimal.valueOf(14.80));
+
+    }
+}
